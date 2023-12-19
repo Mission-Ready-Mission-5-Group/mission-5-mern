6a5e7cca05baf5e3a24e06947f5eac2f7a6cad0c
s <s.looi2@outlook.com>
2023-12-19 16:23:46 +1300

    Add Rentals route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,15 +4,18 @@ import './App.css'
 import Home from './pages/Home'
 import { BrowserRouter, Link, NavLink, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Rentals from './pages/Rentals'
 
 function App() {
 	return (
 		<>
+		{/* <img src={viteLogo} className="logo" alt="Vite logo" />
+	<img src={reactLogo} className="logo react" alt="React logo" /> */}
 			<Navbar />
-			{/* <img src={viteLogo} className="logo" alt="Vite logo" />
-		<img src={reactLogo} className="logo react" alt="React logo" /> */}
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/Rentals" element={<Rentals />} />
+				{/* <Route path="/SelectedProperty" element={<SelectedProperty />} /> */}
 			</Routes>
 		</>
 	);
