14acef04c6e31ae4f314a8a3cd9cb98d0474c19c
s <s.looi2@outlook.com>
2023-12-19 17:08:20 +1300

    Add note about fixing no visible changes issue

--- client/src/pages/SelectedProperty.tsx
+++ client/src/pages/SelectedProperty.tsx
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom"
 
 const SelectedProperty = () => {
 	const {id} = useParams()
-	console.log("id",id)
 	return (
 		<h1>This is the selected property with id </h1>
 	)
