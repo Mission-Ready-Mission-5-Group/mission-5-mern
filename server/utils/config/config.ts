79e5cb8623006dbc1d8d40a19bb56bdab0e06623
s <s.looi2@outlook.com>
2023-12-20 13:10:35 +1300

    Allow environment variable loader to print strings for debugging

--- server/utils/config/config.ts
+++ server/utils/config/config.ts
@@ -5,15 +5,15 @@ dotenv.config();
 
 // Export Config
 export const CONFIG = {
-	PORT: loadEnvironmentVariable(process.env.PORT),
-	MONGO_HOST: loadEnvironmentVariable(process.env.MONGO_HOST),
-	NODE_ENV: loadEnvironmentVariable(process.env.NODE_ENV)
+	PORT: loadEnvironmentVariable(process.env.PORT,"process.env.PORT"),
+	MONGO_HOST: loadEnvironmentVariable(process.env.MONGO_HOST,"MONGO_HOST"),
+	NODE_ENV: loadEnvironmentVariable(process.env.NODE_ENV,"NODE_ENV")
 }
 
 
 // HELPER FUNCTION
-function loadEnvironmentVariable<T>(environmentVariable: T) {
+function loadEnvironmentVariable<T>(environmentVariable: T,string?:string) {
 	if (!environmentVariable)
-		throw new Error("Error: environment variable is NOT defined")
+		throw new Error("Error: environment variable "+string+" is NOT defined")
 	return environmentVariable
 }
